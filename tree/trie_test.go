b53db2853b2feff609d84dfa66a71268fefbd38d
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-10-24 10:30:36 -0300

    Add tests placeholders

---
+++ tree/trie_test.go
@@ -0,0 +1,23 @@
+package tree
+
+import "testing"
+
+func TestCreateTrie(t *testing.T) {
+	trie := tree.CreateTrie()
+	// ...
+}
+
+func TestInsert(t *testing.T) {
+	trie := tree.CreateTrie()
+	// ...
+}
+
+func TestDelete(t *testing.T) {
+	trie := tree.CreateTrie()
+	// ...
+}
+
+func TestSearch(t *testing.T) {
+	trie := tree.CreateTrie()
+	// ...
+}
