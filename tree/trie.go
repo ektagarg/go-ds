ab5ebc2d8cd248ec67db269828fdba2800b2c976
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-10-24 20:06:06 -0300

    Fix reference issues

--- tree/trie.go
+++ tree/trie.go
@@ -19,9 +19,8 @@ type Node struct {
 }
 
 // CreateTrie creates an empty Trie tree and returns it.
-func CreateTrie() Trie {
-	var t Trie
-	return t
+func CreateTrie() *Trie {
+	return &Trie{nil}
 }
 
 // CreateNode creates a new node containing data and prefix string.
@@ -32,7 +31,7 @@ func CreateNode(data, prefix string) *Node {
 
 // Insert appends a node (n) containing data and prefix to the trie.
 // Returns an error if the node already exists.
-func (t Trie) Insert(data, prefix string) error {
+func (t *Trie) Insert(data, prefix string) error {
 
 	if prefix == "" || prefix == nil {
 		return errors.New("Can't insert node with empty prefix")
@@ -47,19 +46,20 @@ func (t Trie) Insert(data, prefix string) error {
 	return t.insertNode(n)
 }
 
-func insertNode(n *Node) error {
+func (t *Trie) insertNode(n *Node) error {
+
 }
 
 // Delete searches for a prefix in the Trie.
 // Removes the node and rearranges the tree if prefix exists.
 // Returns an error if prefix doesn't exist.
-func (t Trie) Delete(prefix string) error {
+func (t *Trie) Delete(prefix string) error {
 }
 
 // Search looks for the node indexed by prefix.
 // Returns a string containing the data if prefix exists.
 // Returns an empty string and error if prefix doesn't exist.
-func (t Trie) Search(prefix string) (string, error) {
+func (t *Trie) Search(prefix string) (string, error) {
 	if t == nil {
 		return "", errors.New("Can't search in nil trie")
 	}
