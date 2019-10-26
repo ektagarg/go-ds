1f94965cc45c98e948c8c86f77754c29586631d2
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-10-25 23:55:02 -0300

    Add more (failing) tests

--- trees/trie/trie_test.go
+++ trees/trie/trie_test.go
@@ -12,6 +12,11 @@ func TestInsert(t *testing.T) {
 	trie.Insert(10, []rune("a"))
 	trie.Insert(11, []rune("b"))
 	trie.Insert(12, []rune("c"))
+
+	trie.Insert(13, []rune("and"))
+	trie.Insert(14, []rune("as"))
+	trie.Insert(15, []rune("assign"))
+	trie.Insert(16, []rune("ah don't know"))
 }
 
 func TestDelete(t *testing.T) {
@@ -19,9 +24,9 @@ func TestDelete(t *testing.T) {
 }
 
 func TestSearch(t *testing.T) {
-	lookup := []string{"a", "b", "c"}
+	lookup := []string{"a", "b", "c", "and", "as", "assign", "ah don't know"}
 
-	expected := []Data{10, 11, 12}
+	expected := []Data{10, 11, 12, 13, 14, 15, 16}
 
 	for i, look := range lookup {
 		ans, err := trie.Search([]rune(look))
